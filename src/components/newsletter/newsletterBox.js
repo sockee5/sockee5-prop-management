021608c50e5e1980af18eaa87ba2bfc33f18ccc3
Code <Code@batc.local>
2018-08-07 08:56:43 -0600

    styled the newsletter box component

--- src/components/newsletter/newsletterBox.js
+++ src/components/newsletter/newsletterBox.js
@@ -6,7 +6,7 @@ export default function NewsletterBox({date}) {
         return (
             <div className='newsletter-box'>
                 <div className='newsletter-box__day'>{date.getDate()}</div>
-                <div className='newsletter-box__month-year'>{months[date.getMonth()]} {props.date.getFullYear()}</div>
+                <div className='newsletter-box__month-year'>{months[date.getMonth()]} {date.getFullYear()}</div>
             </div>
         )
 }
